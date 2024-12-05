706b72bd8800b552dcac1bee43ae34000574105e
kuballa <jziomek669@gmail.com>
2024-12-05 22:45:22 +0100

    leetcode 135 - fail

---
+++ ex/LeetCode2024/135_Candy.py
@@ -0,0 +1,36 @@
+# @algorithm @lc id=135 lang=python3 
+# @title candy
+
+
+from en.Python3.mod.preImport import *
+# @test([1,0,2])=5
+# @test([1,2,2])=4
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        total_candies = n
+        i = 1
+
+        while i < n:
+            if ratings[i] == ratings[i - 1]:
+                i += 1
+                continue
+            
+            current_peak = 0
+            while i < n and ratings[i] > ratings[i - 1]:
+                current_peak += 1
+                total_candies += current_peak
+                i += 1
+            
+            if i == n:
+                return total_candies
+            
+            current_valley = 0
+            while i < n and ratings[i] < ratings[i - 1]:
+                current_valley += 1
+                total_candies += current_valley
+                i += 1
+            
+            total_candies -= min(current_peak, current_valley)
+        
+        return total_candies
